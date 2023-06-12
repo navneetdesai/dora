277d7261bd4ffb5fad49f1af1022ff94b55dcfba
Navneet Desai <navneetdesai44@gmail.com>
2023-06-12 20:12:45 +0530

    Remove global variables by extracting code into classes

--- app/main.py
+++ app/main.py
@@ -15,8 +15,8 @@ app.add_middleware(
 )
 
 models.Base.metadata.create_all(bind=engine)
-app.include_router(user.router)
-app.include_router(auth.router)
+app.include_router(user.DoraUser.router)
+app.include_router(auth.DoraAuth.router)
 
 
 @app.get("/")
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,3 @@
-import distutils.version
-
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
 
@@ -8,17 +6,19 @@ from ..helpers import create_jwt_token, get_user, validate_password
 from ..models import User
 from ..schemas import Authentication
 
-router = APIRouter(tags=["Authentication"])
 
+class DoraAuth:
+    router = APIRouter(tags=["Authentication"])
 
-@router.post("/login")
-def login(credentials: Authentication, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.username == credentials.username).first()
-    if not user or not validate_password(credentials.password, user.password):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials."
-        )
-    jwt_token = create_jwt_token({"username": user.username})
-    user = get_user(jwt_token)
-    print(user)
-    return {"access_token": jwt_token, "token_type": "Bearer"}
+    @staticmethod
+    @router.post("/login")
+    def login(credentials: Authentication, db: Session = Depends(get_db)):
+        user = db.query(User).filter(User.username == credentials.username).first()
+        if not user or not validate_password(credentials.password, user.password):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials."
+            )
+        jwt_token = create_jwt_token({"username": user.username})
+        user = get_user(jwt_token)
+        print(user)
+        return {"access_token": jwt_token, "token_type": "Bearer"}
