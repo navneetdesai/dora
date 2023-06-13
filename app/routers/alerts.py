cb857066a9cd88ffb44d0b4fe4545c6ae258b35b
Navneet Desai <navneetdesai44@gmail.com>
2023-06-13 20:59:01 +0530

    Fix validation bug

--- app/routers/alerts.py
+++ app/routers/alerts.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.db_helper import get_db
 from app.logger import Logger
+from app.models import Alert
 from app.schemas import AlertsCreateRequest
 
 
@@ -15,14 +16,16 @@ class DoraAlert:
     router = APIRouter(tags=["Alerts"])
     logger = Logger(__name__)
 
+    @staticmethod
     @router.post("/alerts", status_code=status.HTTP_201_CREATED)
-    async def create_alert(
-        self, request: AlertsCreateRequest, db: Session = Depends(get_db)
-    ):
-        pass
-        # await self._validate_alerts(request) and self.logger.info("Alerts validated")
+    async def create_alert(request: AlertsCreateRequest, db: Session = Depends(get_db)):
+        dora_alert = DoraAlert()
+        dora_alert._validate_alerts(request)
+        dora_alert.logger.info("Alerts validated")
+        dora_alert.store_alerts(request, db)
+        return None
 
-    async def _validate_alerts(self, request):
+    def _validate_alerts(self, request):
         for alert in request.alerts:
             if not self._validate_alert(alert):
                 raise HTTPException(
@@ -37,11 +40,33 @@ class DoraAlert:
         :param alert: alert to validate
         :return: True if valid, False otherwise
         """
-        is_severity_valid = (
-            alert.severity.lower() in "critical",
-            "high",
-            "medium",
+        is_severity_valid = alert.severity.lower() in [
             "low",
-        )
-        is_coverage_valid = 0 <= alert.coverage < 10, 000
+            "medium",
+            "high",
+            "critical",
+        ]
+        print(type(alert.coverage))
+        is_coverage_valid = 0 <= alert.coverage < 10000
+        print(is_coverage_valid)
         return is_severity_valid and is_coverage_valid
+
+    def store_alerts(self, request, db):
+        try:
+            for alert in request.alerts:
+                alert_ = Alert(
+                    title=alert.title,
+                    description=alert.description,
+                    severity=alert.severity,
+                    coverage=alert.coverage,
+                )
+                db.add(alert_)
+                db.commit()
+                db.refresh(alert_)
+        except Exception as e:
+            self.logger.error(f"Error storing alerts: {e}")
+            db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Error storing alerts: {e}",
+            )
